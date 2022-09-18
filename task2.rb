5488b384ac393c99074922ca2fb307bd024c36e0
Artem Poliakov <artem4250@gmail.com>
2022-09-19 00:50:09 +0300

    task2.rb

---
+++ task2.rb
@@ -0,0 +1,36 @@
+def function(x, n)
+  x, n = x.to_f, n.to_i
+  return nil if x < 0.1 || x > 0.8
+  return nil if (n < 14 && n >= 0) || n > 58
+  result = 0.0
+  if n > 0
+    (1..n).each do |i|
+      result += Math.sin((2*i-1)*x) / (2*i-1)
+    end
+  else
+    e = 0.001
+    i = 1
+    loop do
+      current = Math.sin((2*i-1)*x) / (2*i-1)
+      result += current
+      i += 1
+      break if current.abs < e
+    end
+  end
+  result
+end
+
+print 'Enter x n values (space-separated):'
+str = gets
+exit if str.nil? || str.empty?
+str.chomp!
+x, n = str.split(' ')
+x, n = x.to_f, n.to_i
+print "\nInput:\n x = #{x}\n n = #{n}\n\n"
+
+res = function(x, n)
+if res.nil?
+  print "Error! Parameters out of range.\n"
+else
+  print "Result is ", res, "\n"
+end
